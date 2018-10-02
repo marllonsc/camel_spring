1844d13577bf19153a7872876b2170c6a1397807
marllon.s.costa <marllon.s.costa@EISABA-NT0067.eisa.corp.com>
2018-10-02 15:10:43 -0300

    Organização das Rotas

--- camel-example/src/main/java/com/marllon/sc/camelexample/CustomRouterBuilder.java
+++ camel-example/src/main/java/com/marllon/sc/camelexample/CustomRouterBuilder.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class CustomRouterBuilder extends RouteBuilder {
 
+	
 	@Autowired
 	MyFileProcessor myFileProcessor;
 
@@ -13,20 +14,26 @@ public class CustomRouterBuilder extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
+		
 		myFileProcessor = new MyFileProcessor();
-
 		myProcessorBody = new MyProcessorBody();
 
+		
+		from("file:C:/SpringCamel/inbox").to("file:C:/SpringCamel/outbox");
+		 
+		
 		from("file:C:/SpringCamel/File_inbox?noop=true").choice().when().simple("${file:ext} == 'txt'")
 				.process(myFileProcessor).to("file:C:/SpringCamel/outboxTXT").otherwise()
 				.to("file:C:/SpringCamel/File_outbox_outrosArquivos");
 
+		
 		from("jetty://http://localhost:8888/greeting").log("Received a request").process(myProcessorBody)
 				.to("file:C:/ROUTS_TEST/outboxTXT");
 
 		from("file:C:/SpringCamel/File_inbox/?noop=true").log("Uploading file ...").to(
 				"ftp://172.22.9.3:21?autoCreate=false&username=root&password=123456&passiveMode=true&binary=true\\&resumeDownload=true&localWorkDirectory=target/ftp-work\\&transferLoggingLevel=INFO&transferLoggingIntervalSeconds=1&transferLoggingVerbose=false")
 				.log("Uploaded file !!!");
+		
 
 		from("file:C:/SpringCamel/File_inbox_FTP2/?noop=true").log("Uploading file ...")
 				.to("ftp://root@172.22.9.3:21/ftp/?password=123456&passiveMode=true").log("Uploaded file !!!");
