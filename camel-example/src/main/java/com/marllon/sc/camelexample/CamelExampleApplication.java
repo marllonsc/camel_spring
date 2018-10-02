2d86a7b8cea441d8c7d65fc6ff2ca1be38b10423
marllon.s.costa <marllon.s.costa@EISABA-NT0067.eisa.corp.com>
2018-10-01 23:09:54 -0300

    Pronto para apresentação

--- camel-example/src/main/java/com/marllon/sc/camelexample/CamelExampleApplication.java
+++ camel-example/src/main/java/com/marllon/sc/camelexample/CamelExampleApplication.java
@@ -6,32 +6,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-//@ImportResource("${classpath:spring/camel-context.xml}")
+@ImportResource("${classpath:spring/camel-context.xml}")
 public class CamelExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CamelExampleApplication.class, args);
 	}
 
-	@Bean
-	SpringCamelContext camelContext(ApplicationContext context) {
-		SpringCamelContext camelContext = null;
-
-		try {
-			camelContext = new SpringCamelContext(context);
-			camelContext.addRoutes(createRoutes());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return camelContext;
-	}
-
-	private RouteBuilder createRoutes() {
-		return new CustomRouterBuilder();
-	}
+//	@Bean
+//	SpringCamelContext camelContext(ApplicationContext context) {
+//		SpringCamelContext camelContext = null;
+//
+//		try {
+//			camelContext = new SpringCamelContext(context);
+//			camelContext.addRoutes(createRoutes());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//		return camelContext;
+//	}
+//
+//	private RouteBuilder createRoutes() {
+//		return new CustomRouterBuilder();
+//	}
 
 
 }
