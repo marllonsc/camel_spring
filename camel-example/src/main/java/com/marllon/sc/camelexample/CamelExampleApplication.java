e47b2b7cda4e0540758ccc712529d2da812d591c
Marllon Costa <marllon.costa@sprint.com>
2019-10-17 11:04:16 -0300

    test connection MQ

--- camel-example/src/main/java/com/marllon/sc/camelexample/CamelExampleApplication.java
+++ camel-example/src/main/java/com/marllon/sc/camelexample/CamelExampleApplication.java
@@ -1,15 +1,32 @@
 package com.marllon.sc.camelexample;
 
+import javax.jms.JMSException;
+
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.spring.SpringCamelContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jms.JmsException;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+import com.ibm.mq.jms.MQQueueConnectionFactory;
+import com.ibm.msg.client.wmq.WMQConstants;
+
+//@SpringBootApplication
 //@ImportResource("${classpath:spring/camel-context.xml}")
+@SpringBootApplication
+@RestController
+@EnableJms
 public class CamelExampleApplication {
+	
+	@Autowired
+    private JmsTemplate jmsTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CamelExampleApplication.class, args);
@@ -27,6 +44,57 @@ public class CamelExampleApplication {
 		}
 
 		return camelContext;
+	}
+	
+	@GetMapping("send")
+	String send(){
+	    try{
+	        jmsTemplate.convertAndSend("DEV.QUEUE.1", "Hello World!");
+	        return "OK";
+	    }catch(JmsException ex){
+	        ex.printStackTrace();
+	        return "FAIL";
+	    }
+	}
+	
+	
+//	@Bean
+//	public JmsTemplate jmsQueueTemplate() {
+//		
+//		JmsTemplate jms = new JmsTemplate();
+//		//jms.getConnectionFactory().createConnection(iportSettings.user, iportSettings.password);
+//		return jms;
+//		
+//	}
+	
+	@Bean
+	public MQQueueConnectionFactory mqQueueConnectionFactory(@Autowired IportSettings iportSettings) { 
+	        MQQueueConnectionFactory mqQueueConnectionFactory = new MQQueueConnectionFactory(); 
+	        try { 
+	            mqQueueConnectionFactory.setHostName(iportSettings.host); 
+	            mqQueueConnectionFactory.setQueueManager(iportSettings.queueManager); 
+	            mqQueueConnectionFactory.setPort(Integer.parseInt(iportSettings.port)); 
+	            mqQueueConnectionFactory.setChannel(iportSettings.channel); 
+	            mqQueueConnectionFactory.setTransportType(WMQConstants.WMQ_CM_CLIENT); 
+	            mqQueueConnectionFactory.setCCSID(1208); 
+	            mqQueueConnectionFactory.setStringProperty(WMQConstants.USERID, iportSettings.user);
+	            mqQueueConnectionFactory.setStringProperty(WMQConstants.PASSWORD, iportSettings.password);
+	        } catch (Exception e) { 
+	            e.printStackTrace(); 
+	        } 
+	        return mqQueueConnectionFactory; 
+	    } 
+	
+	
+	
+	@GetMapping("recv")
+	String recv(){
+	    try{
+	        return jmsTemplate.receiveAndConvert("DEV.QUEUE.1").toString();
+	    }catch(JmsException ex){
+	        ex.printStackTrace();
+	        return "FAIL";
+	    }
 	}
 
 	private RouteBuilder createRoutes() {
